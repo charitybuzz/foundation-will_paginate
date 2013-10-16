193dc0276605f3d62472b617356261c61463f0c7
jahrichie <roreilly@charitybuzz.com>
2013-10-16 17:50:58 -0400

    modified helper to allow us to pass params for multiple paginations in one action

--- lib/foundation-will_paginate/helper.rb
+++ lib/foundation-will_paginate/helper.rb
@@ -1,9 +1,9 @@
 module Foundation
   module WillPaginate
-	  module Helper
-	    def foundation_paginate(pages)
-	      will_paginate(pages, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => Foundation::WillPaginate::Renderer, :previous_label => '&laquo;'.html_safe, :next_label => '&raquo;'.html_safe)
-	    end
-	  end
-	end
+    module Helper
+      def foundation_paginate(pages, pagination_for = nil)
+        will_paginate(pages, :params => {:pagination_for => "#{pagination_for}"}, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => Foundation::WillPaginate::Renderer, :previous_label => '&laquo;'.html_safe, :next_label => '&raquo;'.html_safe)
+      end
+    end
+  end
 end
