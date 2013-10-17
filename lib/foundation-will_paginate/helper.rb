84c1dbacae903f9dc4094715dfd9217e66b521e1
jahrichie <roreilly@charitybuzz.com>
2013-10-16 22:24:32 -0400

    only use our params if present

--- lib/foundation-will_paginate/helper.rb
+++ lib/foundation-will_paginate/helper.rb
@@ -2,7 +2,14 @@ module Foundation
   module WillPaginate
     module Helper
       def foundation_paginate(pages, pagination_for = nil)
-        will_paginate(pages, :params => {:pagination_for => "#{pagination_for}"}, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => Foundation::WillPaginate::Renderer, :previous_label => '&laquo;'.html_safe, :next_label => '&raquo;'.html_safe)
+        #be out early if new parm
+        if pagination_for.nil?
+          will_paginate(pages, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => Foundation::WillPaginate::Renderer, :previous_label => '&laquo;'.html_safe, :next_label => '&raquo;'.html_safe)
+        else
+          #else lets run the params, and the only reason i forked this.
+          will_paginate(pages, :params => {:pagination_for => "#{pagination_for}"}, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => Foundation::WillPaginate::Renderer, :previous_label => '&laquo;'.html_safe, :next_label => '&raquo;'.html_safe)
+        end
+
       end
     end
   end
