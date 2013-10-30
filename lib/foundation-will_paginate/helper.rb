0068bdf879be91abf796c553523617ceb8fd7897
jahrichie <roreilly@charitybuzz.com>
2013-10-30 11:50:12 -0400

    update the sort state params if differ from pagiation_for

--- lib/foundation-will_paginate/helper.rb
+++ lib/foundation-will_paginate/helper.rb
@@ -7,6 +7,10 @@ module Foundation
           will_paginate(pages, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => Foundation::WillPaginate::Renderer, :previous_label => '&laquo;'.html_safe, :next_label => '&raquo;'.html_safe)
         else
           #else lets run the params, and the only reason i forked this.
+          #update the sort state to match the pagination state!
+          if params[:state] != pagination_for
+            request.parameters.merge({:state => pagination_for})
+          end
           will_paginate(pages, :params => {:pagination_for => "#{pagination_for}"}, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => Foundation::WillPaginate::Renderer, :previous_label => '&laquo;'.html_safe, :next_label => '&raquo;'.html_safe)
         end
 
